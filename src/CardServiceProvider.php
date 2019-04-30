3f5c16c2c37c033c25101281cb60d18654d58323
Chin Leung <chin@swoo.ca>
2019-04-30 14:29:14 -0400

    Remove the unused routes function

--- src/CardServiceProvider.php
+++ src/CardServiceProvider.php
@@ -16,22 +16,9 @@ class CardServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->booted(function () {
-            $this->routes();
-        });
-
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-countdown', __DIR__.'/../dist/js/card.js');
             Nova::style('nova-countdown', __DIR__.'/../dist/css/card.css');
         });
-    }
-
-    /**
-     * Register the card's routes.
-     *
-     * @return void
-     */
-    protected function routes()
-    {
     }
 }
